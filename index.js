c3d3d43b35567a04b3cf3a901e7a917f17d91d12
RinhBKTrueMission <rinh.tt172783@sis.hust.edu.vn>
2022-01-07 10:31:00 +0700

    up

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const aedes = require('aedes')();
 const server = require('aedes-server-factory').createServer(aedes, {
     ws: true,
 });
-const port = 1883;
+const port = process.env.PORT || 1883;
 
 server.listen(port, function() {
     console.log('server started and listening on port ', port);
